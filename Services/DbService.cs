978be9bc6eab6554b416652080538c762ea7cc09
s19234 <61685731+s19234@users.noreply.github.com>
2020-07-01 12:55:06 +0200

    Add files via upload

---
+++ Services/DbService.cs
@@ -0,0 +1,33 @@
+﻿using KolosProprawkowynr2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KolosProprawkowynr2.Services
+{
+    public class DbService : IDbService
+    {
+        //public
+        ArtistDbContext _context;
+
+        public DbService(ArtistDbContext context)
+        {
+            this._context = context;
+        }
+
+        public Artist_Event GetArtist(int id)
+        {
+            try
+            {
+                return null;
+            }
+            catch(SqlException)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Services/IDbService.cs
@@ -0,0 +1,13 @@
+﻿using KolosProprawkowynr2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KolosProprawkowynr2.Services
+{
+    public interface IDbService
+    {
+        public Artist_Event GetArtist(int id);
+    }
+}
